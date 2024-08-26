5e2dd5b00ae2bab677c0bee58bebdfee8520c085
Oscar Crowe <139089439+oscar-crowe22@users.noreply.github.com>
2024-08-27 10:23:56 +1200

    Update test.cpp

--- C++/test.cpp
+++ C++/test.cpp
@@ -7,7 +7,7 @@ bool imageSize(const std::string& filename, char** &art, int &artWidth, int &art
     // test the size of the array to determine height / width
     // if one fails crash
     art = readFileTo2DArray(filename, artWidth, artHeight);
-    if(artWidth == 660 && artHeight == 68){
+    if(artWidth == 660 && artHeight == 64){
         return true;
     }else{
         throw std::runtime_error("Incorrect image size in " + filename + ", expcted (660 x 63) -> UTF8");
