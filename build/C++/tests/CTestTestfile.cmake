00c83fddcef115f6335fb58c6a8494adc8df6d28
oscar-crowe22 <oscardjcrowe@gmail.com>
2024-08-29 11:15:01 +1200

    please

---
+++ build/C++/tests/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/croos989/Desktop/commandLine/C++/tests
+# Build directory: C:/Users/croos989/Desktop/commandLine/build/C++/tests
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(test_executable "test_executable")
+set_tests_properties(test_executable PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/croos989/Desktop/commandLine/C++/tests/CMakeLists.txt;13;add_test;C:/Users/croos989/Desktop/commandLine/C++/tests/CMakeLists.txt;0;")
---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/croos989/Desktop/commandLine
+# Build directory: C:/Users/croos989/Desktop/commandLine/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("C++")
+subdirs("C++/tests")
