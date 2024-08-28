1b3f7806091121686a58f4847af523ea4319749c
oscar-crowe22 <oscardjcrowe@gmail.com>
2024-08-29 11:17:33 +1200

    fsdfd

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/croos989/Desktop/commandLine
-# Build directory: C:/Users/croos989/Desktop/commandLine/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("C++")
-subdirs("C++/tests")
