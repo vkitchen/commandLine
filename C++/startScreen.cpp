62eaa4adc5e07217ce16851478f54f6d691288fa
oscar-crowe22 <oscardjcrowe@gmail.com>
2024-09-13 17:59:19 +1200

    sdfsdf

--- C++/startScreen.cpp
+++ C++/startScreen.cpp
@@ -2,6 +2,8 @@
 #include "startScreen.hpp"
 #include "renderArt.hpp"
 #include "optionsBox.hpp"
+#include "terminalHelpers.hpp"
+#include <windows.h>
 
 /*!
  * @file
@@ -14,18 +16,53 @@ void startScreen() {
     int startYBox = (totalConsoleHeight * 1 / 3) + 1;
     int endYBox = totalConsoleHeight - 1;
 
+
+
+    const double probability = 0.1; // 10% chance
+    const int arraysize = 26860;
+    std::vector<std::vector<char>> array(85, std::vector<char>(316));
+
     std::string title = "art/startScreen/Escape-Owheo.txt";
+
+    // Create a 2D vector of chars
+    std::srand(static_cast<unsigned>(1));
+    for(int i = 0; i < 85; i++){
+        for(int j = 0 ; j < 316; j++){
+            float randomchoice = static_cast<float>(std::rand()) / RAND_MAX;
+
+            if(randomchoice < 0.1){
+                array[i][j] = '1' + (std::rand() % 9);
+            }else{
+                array[i][j] = ' ';
+            }
+        }
+
+    }
+    char* ptrarray[85];
+    for(int i = 0; i< 85; i++){
+        ptrarray[i] = array[i].data();
+    }
+    int frames = 0;
+    while(frames < 1){
+        for(int i = 0; i < 85; i++){
+            for(int j = 0 ; j < 316; j++){
+                std::cout << ptrarray[i][j];
+                
+            }
+             Sleep(10);
+        }
+        frames ++;
+    }
     loadArt(title, art, artWidth, artHeight);
     int startXTitle = (totalConsoleWidth - artWidth/3) / 2;
     int startYTitle = (startYBox - artHeight) / 2;
     renderArt(art, artWidth, artHeight, startXTitle, startYTitle);
-
     std::vector<std::string> options = {"NEW GAME", "LOAD GAME", "SR STATS", "SETTINGS", "EXIT"};
 
     int selectedIndex = renderOptionsBox(startXBox, endXBox, startYBox, endYBox, options);
 
     switch (selectedIndex) {
-        case 0:
+        case 0: 
             // New Game selected, break to return to main.cpp and enter main game loop
             input = ' ';
             break;
